50ef5ecc057c1e8f06ad6df141233e632c31a767
Ahmed Kechicheb <ahmedkechicheb13@gmail.com>
2023-03-19 11:57:04 +0100

    test isLoading isError when useQuery

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,10 +10,13 @@ function App() {
     queryKey: ["posts"],
     queryFn: () => wait(1000).then(() => [...POSTS]),
   });
-
+    if (postsQuery.isLoading) return <h1>Loading...</h1>;
+  if (postsQuery.isError) {
+    return <pre>{JSON.stringify(postsQuery.error)}</pre>;
+  }
   return (
     <div className="App">
-      {postsQuery.data && postsQuery.data.map((post) => (
+      { postsQuery.data.map((post) => (
         <div key={post.id}>{post.title}</div>
       ))}
     </div>
