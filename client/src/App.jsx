1a1a56353ac9c6013950e15c31ff0ec4c7d98325
Ahmed Kechicheb <ahmedkechicheb13@gmail.com>
2023-03-19 11:55:10 +0100

    test useQuery

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,25 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+const POSTS = [
+  { id: 1, title: "Post 1" },
+  { id: 2, title: "Post 1" },
+];
+
 function App() {
+  const queryClient = useQueryClient();
+  const postsQuery = useQuery({
+    queryKey: ["posts"],
+    queryFn: () => wait(1000).then(() => [...POSTS]),
+  });
+
   return (
     <div className="App">
-      <h1>Ahmed Kechicheb</h1>
+      {postsQuery.data && postsQuery.data.map((post) => (
+        <div key={post.id}>{post.title}</div>
+      ))}
     </div>
   );
 }
-
+function wait(duration) {
+  return new Promise((resolve) => setTimeout(resolve, duration));
+}
 export default App;
