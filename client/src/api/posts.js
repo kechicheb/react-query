eec9d4f001b21713429e752f22302a94f4f568d8
Ahmed Kechicheb <ahmedkechicheb13@gmail.com>
2023-03-19 18:52:49 +0100

    function createPost

--- client/src/api/posts.js
+++ client/src/api/posts.js
@@ -7,3 +7,13 @@ export function getPosts() {
 export function getPost(id) {
   return axios.get(`http://localhost:3000/posts/${id}`).then((res) => res.data);
 }
+export function createPost({ title, body }) {
+  return axios
+    .post("http://localhost:3000/posts", {
+      title,
+      body,
+      userId: 1,
+      id: Date.now(),
+    })
+    .then((res) => res.data);
+}
