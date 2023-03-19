13361d55d66bbece0710fb62e6a4e6be9115ad69
Ahmed Kechicheb <ahmedkechicheb13@gmail.com>
2023-03-19 12:15:27 +0100

    add useMutation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,15 +10,24 @@ function App() {
     queryKey: ["posts"],
     queryFn: () => wait(1000).then(() => [...POSTS]),
   });
-    if (postsQuery.isLoading) return <h1>Loading...</h1>;
+  const newPostMutation = useMutation({
+    mutationFn: (title) => {
+      return wait(1000).then(() => POSTS.push({ id:crypto.randomUUID(), title }));
+    },
+    onSuccess: () => queryClient.invalidateQueries(["posts"]),
+  });
+  if (postsQuery.isLoading) return <h1>Loading...</h1>;
   if (postsQuery.isError) {
     return <pre>{JSON.stringify(postsQuery.error)}</pre>;
   }
   return (
     <div className="App">
-      { postsQuery.data.map((post) => (
+      {postsQuery.data.map((post) => (
         <div key={post.id}>{post.title}</div>
       ))}
+      <button disabled={newPostMutation.isLoading} onClick={() => newPostMutation.mutate("new post")}>
+        ADD NEW
+      </button>
     </div>
   );
 }
